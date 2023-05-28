f4c56e86771d52fdb27a6823dba4dd64d14dadbe
KevinRodriguesSilva <kevin.ponas@gmail.com>
2023-05-27 21:22:30 -0300

    Estilização e estruturação do site da cruzadinha

---
+++ public/js/animacoes.js
@@ -0,0 +1,60 @@
+function expandir(){
+    var sidebar = document.getElementById('sidebar');
+    var menu = document.getElementById('menu');
+    var menuInativo = document.getElementById('menu-desativado');
+
+    if(sidebar.classList.length > 1){
+        sidebar.classList.replace('inative', 'active');
+    }else{
+        sidebar.classList.add('active')
+    }
+
+    for (var i = 1; i < 17; i++) {
+        var questoes = document.getElementById(`question${i}`);
+        questoes.classList.add('question-ativada')
+    }
+
+    if(menu.classList[1] == 'ativada'){
+        menu.classList.replace('ativada', 'desativada');
+        menuInativo.classList.replace('desativada', 'ativada')
+    }
+
+
+    console.log('console do menu na func minimizar:'+ menu.classList[1]);
+    console.log('console do menuInativo na func minimizar:'+ menuInativo.classList[1]);
+}
+
+function minimizar(){
+    var sidebar = document.getElementById('sidebar');
+    var menu = document.getElementById('menu');
+    var menuInativo = document.getElementById('menu-desativado');
+
+
+    if(sidebar.classList[1] == 'active'){
+        sidebar.classList.replace('active', 'inative');
+    }
+
+    if(menuInativo.classList[1] == 'ativada'){
+        menuInativo.classList.replace('ativada', 'desativada');
+        menu.classList.replace('desativada', 'ativada')
+    }
+
+    for (var i = 1; i < 17; i++) {
+        var questoes = document.getElementById(`question${i}`);
+        questoes.classList.remove('question-ativada')
+    }
+
+
+    console.log('console do menu na func minimizar:'+ menu.classList[1]);
+    console.log('console do menuInativo na func minimizar:'+ menuInativo.classList[1]);
+}
+
+function movimentar(){
+    var menuAtivo = document.getElementById('menu')
+
+    if(menuAtivo.classList[1] == 'ativada'){
+        expandir();
+    }else{
+        minimizar();
+    }
+}
